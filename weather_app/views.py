657db9256e9df717493f41d31fa11af345d0e7ae
Jekmen1 <jeko.kobakidze@gmail.com>
2024-04-15 19:40:32 +0400

    first commit

---
+++ weather_app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import render
+import requests, json
+# Create your views here.
+class WeatherApi(APIView):
+
+
+    # def weather(self):
+    #     API_KEY = "509d1236aab16bb823897a206a5b2fd0"
+    #     city_name = 'London'
+    #     url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&units={'metric'}"
+    #     response = requests.get(url)
+    #     j = json.loads(response.text)
+    #     self.temp = j['main']['temp']
+    def get(self, request, temp):
+        API_KEY = "509d1236aab16bb823897a206a5b2fd0"
+        city_name = temp
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&units={'metric'}"
+        response = requests.get(url)
+        j = json.loads(response.text)
+        city = j['main']['temp']
+        return Response({"Your city's weather": city})
